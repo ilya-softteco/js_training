ae9dabb038b04b2ac7c24003b7f8589fb351027a
Martinkevich <i.martinkevich@softteco.com>
2024-05-10 15:46:49 +0300

    mongo

--- nest-js/src/main.ts
+++ nest-js/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {LogGlobalGuard} from "./guard/LogGlobalGuard";
 import {LogGlobalInterceptor} from "./interceptor/LogGlobalInterceptor";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,13 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(3000);
+
   //app.close();
 }
 bootstrap();
